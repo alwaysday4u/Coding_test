ef4875a482fbb1537be0d1330078d841b593381d
Son Lina <139030224+alwaysday4u@users.noreply.github.com>
2023-10-05 19:58:24 +0900

    Create 피보나치 수.py

---
+++ Programmers/Level 2/피보나치 수.py
@@ -0,0 +1,9 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/12945
+'''
+
+def solution(n):
+    a, b = 0, 1
+    for i in range(2,n+1):
+        a,b = b, a+b
+    return b%1234567
